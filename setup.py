6af679a4520bfa45ad8b64fd5d5823774eb80da7
Peter Schilke <schilke@ph1.uni-koeln.de>
2024-03-29 18:02:03 +0100

    changed error handling so that it doens't calculate the spectra if the mctherm fails

--- setup.py
+++ setup.py
@@ -1,33 +1,32 @@
 from model_setup_lines import model_setup_lines as model
 import astropy.constants as const
 import os
+import sys
 
-sdens = "1e10"
+sdens = "1e9"
 dens = float(sdens)
-lum = 1e5
-ri = 100
-ro = 5000
-radius = 500
+lum = 1e4
+ri = 50
+ro = 2000
+radius = 100
 nradial=100
-nlam=1000
+nlam=2000
 vin=1 # scaling factor for infall
 # step function for abundance of molecule
-Tlow = 10
-Thigh = 2000
-abunch3cn=1e-7
+Tlow = 50
+Thigh = 800
+abunch3cn=1e-8
 wls=[1360, 870, 450, 350]
 
-prho=4.0 #power law index
+prho=2.0 #power law index
 test=model(dens,prho,ri=ri,ro=ro,nphot=100000,radius=radius,nradial=nradial,lum=lum)
-r_dev = 2
-phi_dev = 4
+r_dev = 0.0
+phi_dev = 0.1
 test.add_gaussian_variations(r_dev, phi_dev)
 
-#write input filepwd
+#write input file
 mrw=False
 
-incl = 0
-phi = 0
 
 test.write_input(mrw=mrw)
 #run model
@@ -36,23 +35,63 @@ try:
     out = test.calculate_model(ncores=20)
     print (f'stdout: {out.stdout}')
     print (f'stderr: {out.stderr}')
+    with open(f'{dir}.log', 'w') as logfile:
+        logfile.write(f'stdout:\n {out.stdout}')
+        logfile.write(f'stderr:\n {out.stderr}')
+
+except Exception as e:
+    print (f'stdout: {out.stdout}')
+    print (f'stderr: {out.stderr}')
+    with open(f'{dir}.log', 'w') as logfile:
+        logfile.write(f'stdout:\n {out.stdout}')
+        logfile.write(f'stderr:\n {out.stderr}')
+        logfile.write(f'Exception: {e}')
+    print (e)
+    sys.exit(1)
+
+
+try:
     test.add_lines(vin=vin, Tlow=Tlow, Thigh=Thigh, abunch3cn=abunch3cn)
     test.make_vtk()
+ 
+    incl = 0
+    phi = 0
     test.make_cube(nlam=nlam, incl = incl, phi = phi, ncores=20)
     fitsfile = f'{dir}_incl={incl}_phi={phi}.fits'
     test.make_fits(fitsfile)
+
+    incl = 0
+    phi = 90
+    test.make_cube(nlam=nlam, incl = incl, phi = phi, ncores=20)
+    fitsfile = f'{dir}_incl={incl}_phi={phi}.fits'
+    test.make_fits(fitsfile)
+
+    incl = 90
+    phi = 0
+    test.make_cube(nlam=nlam, incl = incl, phi = phi, ncores=20)
+    fitsfile = f'{dir}_incl={incl}_phi={phi}.fits'
+    test.make_fits(fitsfile)
+
+    incl = 90
+    phi = 90
+    test.make_cube(nlam=nlam, incl = incl, phi = phi, ncores=20)
+    fitsfile = f'{dir}_incl={incl}_phi={phi}.fits'
+    test.make_fits(fitsfile)
+
+
 #   these functions take some time and could benefit from multithreading too.
 #   test.make_synth_maps(wls)
 #    test.make_tau_surface(wls)
 
-    with open(f'{dir}.log', 'w') as logfile:
+    with open(f'{dir}.log', 'a+') as logfile:
         logfile.write(f'stdout:\n {out.stdout}')
         logfile.write(f'stderr:\n {out.stderr}')
  
-    cmd = f'mkdir {dir}; cp *.inp *.dat *.out *vtk {dir}; mv *.fits *log {dir}'
+    cmd = f'mkdir {dir}; cp *.inp  *.out {dir}; mv *.fits *log *vtk *dat {dir}'
     os.system(cmd)
 except Exception as e:
     print (e)
+    sys.exit(1)
 
 # write code to read a file
 
